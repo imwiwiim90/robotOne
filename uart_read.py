c6bfe082791a6400a58e518f5393639721888201
imwiwiim90 <imwiwiim90@gmail.com>
2018-03-18 15:26:47 -0500

    d

--- uart_read.py
+++ uart_read.py
@@ -3,9 +3,12 @@ import time
 
 wiringpi.wiringPiSetup()
 serial = wiringpi.serialOpen('/dev/ttyS0',9600)
-
+if serial == -1:
+	print 'error opening serial at /dev/ttyS0'
+	sys.exit(0)
+print 'serial opened'
 i = 0
 while True:
-	if (wiringpi.serialDataAvail(serial)):
+	if (wiringpi.serialDataAvail(serial) > 0):
 		line = wiringpi.serialGetChar(serial)
 		print line
