9fd629ea88129b0b3608583f090ed8b5d8ea0423
imwiwiim90 <imwiwiim90@gmail.com>
2018-03-18 20:00:00 +0000

    soft changes

--- uart_test.py
+++ uart_test.py
@@ -2,10 +2,10 @@ import wiringpi2 as wiringpi
 import time
 
 wiringpi.wiringPiSetup()
-serial = wiringpi.serialOpen('/dev/ttyAMA0',9600)
+serial = wiringpi.serialOpen('/dev/ttyS0',9600)
 
 i = 0
 while True:
 	wiringpi.serialPuts(serial,str(i%2))
-	time.sleep(1)
+	time.sleep(0.1)
 	i+=1
