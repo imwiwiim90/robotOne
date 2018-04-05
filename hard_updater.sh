16f0a6dc84a36413797ce83f452c615b8cfe2c15
imwiwiim90 <imwiwiim90@gmail.com>
2018-04-04 23:47:13 -0500

    as

--- hard_updater.sh
+++ hard_updater.sh
@@ -1,5 +1,5 @@
 cd /home/pi/Desktop/robotOne
-h | grep -E "python|sudo" | awk '{print $2}' | sudo xargs kill
+ps auc | grep -E "python|sudo" | awk '{print $2}' | sudo xargs kill
 git reset --hard
 git pull origin master
 sudo python robot/main.py
